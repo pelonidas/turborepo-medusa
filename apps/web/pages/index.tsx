50f1eab9229622c93755596adbdfe40bafa13978
pelonidas <janx803y@easv365.dk>
2022-12-06 12:16:09 +0100

    Git init

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,26 @@
+import { MedusaProvider } from "medusa-react";
+import { QueryClient } from "react-query";
 import { Button } from "ui";
 
 export default function Web() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        staleTime: 30000,
+        retry: 1,
+      },
+    },
+  });
   return (
-    <div>
-      <h1>Web</h1>
-      <Button />
-    </div>
+    <MedusaProvider
+      queryClientProviderProps={{ client: queryClient }}
+      baseUrl={"http://localhost:9000/"}
+    >
+      <div>
+        <h1>Web</h1>
+        <Button />
+      </div>
+    </MedusaProvider>
   );
 }
